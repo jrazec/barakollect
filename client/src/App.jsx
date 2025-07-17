8abf09b92dd0a651ee9d5a7014d4dfe99db848a1
jrazec <johnrazeagno@gmail.com>
2025-07-17 23:30:25 +0800

    test: link client -> backend(apps.users)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/users/test/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("API fetch failed:", err));
+  }, []);
 
   return (
     <>
@@ -16,15 +23,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>{message}</h1>
+    
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
