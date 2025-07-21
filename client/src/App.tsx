14d3d5a961b1b4d928c5abc50b7edff6c72d44c2
jrazec <johnrazeagno@gmail.com>
2025-07-22 01:14:50 +0800

    feat: implement routing; include temp routes & files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ClientLayout from "./layouts/ClientLayout";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
   return (
-    <>
-      <ClientLayout />
-    </>
+    <Routes>
+      <Route path="/" element={<ClientLayout />}>
+        <Route path="dashboard" element={<Home />} />
+        <Route path="upload-image" element={<Login />} />
+        <Route path="gallery" element={<Home />} />
+        <Route path="farm-map" element={<Login />} />
+        <Route path="notifications" element={<Home />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
