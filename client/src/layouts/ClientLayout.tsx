4940cec4116e19b7978a7904ad2b5e424322f4c9
jrazec <johnrazeagno@gmail.com>
2025-07-21 19:09:40 +0800

    feat: add sidebar nav, design, layout

---
+++ client/src/layouts/ClientLayout.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import SideBar from '../components/SideBar'
+import NavBar from '../components/NavBar'
+import '../assets/styles/global.css'
+export default function ClientLayout() {
+  const [showSideBar, setShowSideBar] = useState<boolean>(true);
+  const logo: string[] = [
+    "/src/assets/images/barakollect_logo.svg",
+    "/src/assets/images/logo.svg"
+  ];
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-1 overflow-auto">
+        <SideBar show={showSideBar} />
+        <div className="flex-column text-white w-full h-full">
+          BODY
+        </div>
+      </div>
+    </div>
+  );
+}
