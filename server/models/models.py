938c7a57b6e213a70d152c2769ff84290d9034df
jrazec <johnrazeagno@gmail.com>
2025-08-27 21:43:17 +0800

    refactor: update UserImage to have primary key instead of composite

--- server/models/models.py
+++ server/models/models.py
@@ -107,13 +107,15 @@ class Image(models.Model):
 
 
 class UserImage(models.Model):
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     is_deleted = models.BooleanField(default=False)
 
     class Meta:
         db_table = "user_images"
-        unique_together = ("user", "image")
+        unique_together = ('user', 'image')
+        managed = False
 
 
 class Annotation(models.Model):
