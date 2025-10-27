1273d2e707be3b1d5cb5357796b4e421de1492ca
auclinn <142300786+auclinn@users.noreply.github.com>
2025-10-28 07:28:11 +0800

    feat: update logo section layout and side nav minimization

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,16 +1,16 @@
-import { ArrowLeftIcon, ArrowRightFromLine } from "lucide-react"
+import { ArrowLeftIcon, ArrowRightFromLine, PanelLeftClose} from "lucide-react"
 
 const NavBar = ({showSideBar,setShowSideBar, logo}:{showSideBar:boolean,setShowSideBar:Function,logo:string[]}) => {
    return (
      <nav className="headerNav text-white pl-4 w-full z-10 flex justify-between items-center">
         <div className="flex items-center">
             <div className="logoSection">
-                <div className={`logo ${showSideBar ? "" : "collapsed"}`}>
+                <div className={`logo ${showSideBar ? "" : "collapsed"} cursor-pointer`}>
                     <img src={logo[showSideBar ? 0 : 1]} alt="logo" onClick={() => showSideBar ? "" : setShowSideBar(!showSideBar)} />
                 </div>
-                <div className={`minimize ${showSideBar ? "" : "collapsed"}`} onClick={() => setShowSideBar(!showSideBar)}>
-                    <div className="bg-black text-white p-1 rounded-md">
-                        {showSideBar ? <ArrowLeftIcon /> : <ArrowRightFromLine />}
+                <div className={`minimize ${showSideBar ? "" : "collapsed"} cursor-pointer`} onClick={() => setShowSideBar(!showSideBar)}>
+                    <div className="bg-white text-gray-700 p-1 rounded-md">
+                        {showSideBar ? <PanelLeftClose /> : ""}
                     </div>
                 </div>
             </div>
