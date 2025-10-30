27f7744f3f01eaafff30d8f1a702cb1278164ee7
auclinn <142300786+auclinn@users.noreply.github.com>
2025-10-31 02:39:45 +0800

    feat: refine tab options in researcher image upload

--- client/src/components/UploadSamples.tsx
+++ client/src/components/UploadSamples.tsx
@@ -19,7 +19,7 @@ const UploadSamples: React.FC = () => {
           title="Upload Bean Images"
           subtitle="Upload coffee bean images for analysis and contribute to our research database"
         />
-        <TabComponent activeTab={activeTab} onTabChange={setActiveTab} tabs={['Predict Image','Submit Image','Find Largest Bean']} />
+        <TabComponent activeTab={activeTab} onTabChange={setActiveTab} tabs={['Predict Image','Find Largest Bean']} />
         <UploadBodySection activeTab={activeTab} onFilesSelected={handleFiles} />
       </div>
     </PageContainer>
