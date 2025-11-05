02d974ce86f6ffb7b3a7a9b527359a0f9bf15dc6
auclinn <142300786+auclinn@users.noreply.github.com>
2025-11-05 09:39:55 +0800

    feat: implement glass surface to navbar; update styles for improved navigation appearance

--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -6,6 +6,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import logo1 from "@/assets/images/barakollect_logo.svg";
 import logo2 from "@/assets/images/logo.svg";
+import GlassSurface from "@/components/GlassSurface";
 
 export default function AdminLayout() {
   const [showSideBar, setShowSideBar] = useState<boolean>(true);
@@ -19,13 +20,32 @@ export default function AdminLayout() {
   if (!user) return <Navigate to="/login" replace />;
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex-shrink-0">
-        <NavBar logo={logo} showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
+    <div className="relative flex flex-col h-screen">
+      {/* Glass Nav */}
+      <div className="absolute top-0 left-0 w-full z-50">
+        <GlassSurface 
+          width="100%" 
+          backgroundOpacity={0.5}
+          borderRadius={0} 
+          displace={5}
+          redOffset={40}
+          blueOffset={10}
+          greenOffset={20}
+          blur={20}
+          brightness={50}
+          saturation={1.5}
+          distortionScale={-200}
+        >
+          <NavBar logo={logo} showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
+        </GlassSurface>
       </div>
-      <div className="flex flex-1 h-[calc(100vh-64px)]">
-        <SideBar show={showSideBar} role={role} user={user}/>
-        <div className="flex-1 h-full overflow-y-auto text-black">
+
+      {/* Main Content */}
+      <div className="flex flex-1 h-full">
+        <div className="pt-[70px]">
+          <SideBar show={showSideBar} role={role} user={user} />
+        </div>
+        <div className="flex-1 pt-[64px] overflow-y-auto text-black">
           <Outlet />
         </div>
       </div>
